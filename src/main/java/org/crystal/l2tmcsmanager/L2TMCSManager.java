2df6023f014096821f30fc3fc4e86540b79192cc
Crystal Moling <clovermoling@gmail.com>
2022-02-11 15:57:49 +0800

    Update

--- src/main/java/org/crystal/l2tmcsmanager/L2TMCSManager.java
+++ src/main/java/org/crystal/l2tmcsmanager/L2TMCSManager.java
@@ -2,25 +2,21 @@ package org.crystal.l2tmcsmanager;
 
 import okhttp3.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.crystal.link2telegram.GetUpdateEvent;
 import org.crystal.link2telegram.Link2telegram;
-import org.crystal.link2telegram.Link2telegramAPI;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class L2TMCSManager extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
-        try {
-            Debug();
-        } catch (Throwable ignored) { }
-        this.getLogger().info(Arrays.toString(Link2telegram.L2tAPI().getServerStatus()));
+        getServer().getPluginManager().registerEvents(this, this);
         this.getLogger().info("L2TMCSManager Enabled!");
     }
 
@@ -29,23 +25,12 @@ public class L2TMCSManager extends JavaPlugin implements Listener {
         this.getLogger().info("L2TMCSManager Disabled!");
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     private void GetUpdateListener(GetUpdateEvent event){
-        this.getLogger().info(event.GetMessage());
         if(Objects.equals(event.GetMessage(), "/restart")){
-            Link2telegram.L2tAPI().sendFormatedMsg("收到重启命令","Info");
+            Link2telegram.L2tAPI().sendFormatedMsg("[Event]收到重启命令","Info");
             RestartServer();
         }
-    }
-
-    private void Debug() throws Throwable {
-        Logger("getUpdatedText: " + Link2telegram.L2tAPI().getUpdatedText());
-        Logger("getServerStatus: " + Link2telegram.L2tAPI().getServerStatus());
-        Logger("getServerTPS: " + Link2telegram.L2tAPI().getServerTPS());
-    }
-
-    private void Logger(Object Log){
-        this.getLogger().info(Log.toString());
     }
 
     private void RestartServer(){
@@ -61,16 +46,19 @@ public class L2TMCSManager extends JavaPlugin implements Listener {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                Link2telegram.L2tAPI().sendFormatedMsg("服务器重启失败","Warn");
+                Link2telegram.L2tAPI().sendFormatedMsg("服务器重启失败,错误原因:\n" + getExceptionType(e),"Warn");
             }
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response){
                 if (response.isSuccessful()) {
                     Link2telegram.L2tAPI().sendFormatedMsg("服务器重启","Status");
                 } else {
-                    Link2telegram.L2tAPI().sendFormatedMsg("服务器重启失败","Warn");
+                    Link2telegram.L2tAPI().sendFormatedMsg("服务器重启失败,错误原因:\n" + response.code(),"Warn");
                 }
             }
         });
+    }
+    private static Throwable getExceptionType(Exception e){
+        return e;
     }
 }
